e1dfbb7854af8c92a3dae0c814075a5973e59f36
Youichi-Higa <yo1_1023@yahoo.co.jp>
2023-02-13 22:25:18 +0900

    feat: メイン部分をコンポーネントとして切り出し

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useState } from 'react';
-import { EditBtn } from 'src/components/atoms/buttons';
-import { MemoTitle } from 'src/components/atoms';
-import { Sidebar } from 'src/components/molecules';
-import { icons } from 'src/paths/icons';
+import { Main, Sidebar } from 'src/components/molecules';
 import '@fontsource/noto-sans-jp';
 
 function App() {
@@ -40,34 +37,7 @@ function App() {
           selectMemo={selectMemo}
         />
 
-        {/* メモ作成・編集エリア */}
-        <div className="col-span-4 min-h-screen pt-[30px] px-10">
-          <div className="bg-light rounded-xl h-[calc(100vh_-_94px)] p-[30px]">
-            {/* タイトル */}
-            <div className="flex justify-between items-center mb-5">
-              <p className="text-2xl font-bold w-full">{memoList[0].title}</p>
-              <div className="w-20 h-10">
-                <EditBtn />
-              </div>
-            </div>
-
-            {/* メモ */}
-            <div className="h-[calc(100vh_-_214px)] flex justify-between gap-5">
-              <div className="w-full rounded-xl bg-white pt-[30px] px-[30px] overflow-auto">
-                <p>{memoList[0].body}</p>
-              </div>
-              <div className="w-20 h-10">
-                <EditBtn />
-              </div>
-            </div>
-          </div>
-
-          {/* フッター */}
-          <div className="h-16 flex justify-between items-center">
-            <p className="text-xs">Copyright © 2021 Sample</p>
-            <p className="text-xs">運営会社</p>
-          </div>
-        </div>
+        <Main memoList={memoList} />
       </div>
     </div>
   );
